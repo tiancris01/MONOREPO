f42b811985c36be6e7c0f5746946c16a1fbd710e
Daniel osorio <tiancris01@gmail.com>
2025-11-26 19:09:48 -0500

    CHORE: Update development environment files

--- apps/epam_tech_interview/test/widget_test.dart
+++ apps/epam_tech_interview/test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:epam_tech_interview/navigation/navigation_2_0_pages_api/material_nav_2_0.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:epam_tech_interview/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyAppNavigation20());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

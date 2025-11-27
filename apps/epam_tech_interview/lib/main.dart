f17ce2f55ccbc3565cd5d5a5a7f5c62f8b9e449e
Daniel osorio <tiancris01@gmail.com>
2025-11-26 20:43:46 -0500

    CHORE: remove epam_tech_interview app files

--- apps/epam_tech_interview/lib/main.dart
+++
@@ -1,4 +0,0 @@
-import 'package:epam_tech_interview/navigation/navigation_2_0_pages_api/material_nav_2_0.dart';
-import 'package:flutter/material.dart';
-
-void main() => runApp(const MyAppNavigation20());
